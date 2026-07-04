b575287a2f5d8c01b6e03ef01c7583e78cbf29ff
rj-roy <dpjdeveloper.me@gmainl.com>
2026-07-05 01:54:35 +0600

    refactor(getMenuByQuery)

--- controllers/menu.controller.js
+++ controllers/menu.controller.js
@@ -8,11 +8,20 @@ export const getMenu = async (req, res) => {
 
 export const getMenuByQuery = async (req, res) => {
     const { menuCollection } = getCollections();
-    const category = req.query.category;
 
-    const result = await menuCollection.find({
-        categoryName : category
-    }).toArray();
+    const { category, dietaryTags } = req.query;
+    const query = {};
 
+    if (category) {
+      query.categoryName = { $regex: `^${category}$`, $options: "i" };
+    };
+
+    if (dietaryTags) {
+      query.dietaryTags = {
+        $all: dietaryTags.split(","),
+      };
+    };
+
+    const result = await menuCollection.find(query).toArray();
     res.send(result);
 };
